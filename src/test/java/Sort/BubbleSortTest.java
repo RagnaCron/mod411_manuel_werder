45aa59cc432e9b3971aed056061333ab79f600a9
manuel.werder <manuel.werder@csbe.ch>
2019-10-17 11:01:36 +0200

    Test for BubbleSort on String and Integer

--- src/test/java/Sort/BubbleSortTest.java
+++ src/test/java/Sort/BubbleSortTest.java
@@ -6,13 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSortTest {
 
+	private BubbleSort<String> stringBubbleSort = new BubbleSort<String>();
+	private BubbleSort<Integer> integerBubbleSort = new BubbleSort<Integer>();
+
 	@Test
 	void StringSort() {
-
+		String[] list = {"q", "c", "h", "d", "p", "x"};
+		String[] expectedList = {"c", "d", "h", "p", "q", "x"};
+		stringBubbleSort.sort(list);
+		assertArrayEquals(expectedList, list);
 	}
 
 	@Test
 	void IntegerSort() {
-
+		Integer[] list = {5, 8, 3, 6, 1, 9, 2, 7, 4, 0};
+		Integer[] expectedList = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		integerBubbleSort.sort(list);
+		assertArrayEquals(expectedList, list);
 	}
 }
