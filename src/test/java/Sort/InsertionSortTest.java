e7c94e9f17f335cacf7dd0b1ca61ed2d20ac20f9
manuel.werder <manuel.werder@csbe.ch>
2019-10-17 11:06:06 +0200

    Test for InsertionSort on String and Integer

--- src/test/java/Sort/InsertionSortTest.java
+++ src/test/java/Sort/InsertionSortTest.java
@@ -4,24 +4,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BubbleSortTest {
+class InsertionSortTest {
 
-	private BubbleSort<String> stringBubbleSort = new BubbleSort<String>();
-	private BubbleSort<Integer> integerBubbleSort = new BubbleSort<Integer>();
+	private InsertionSort<String> stringInsertionSort = new InsertionSort<String>();
+	private InsertionSort<Integer> integerInsertionSort = new InsertionSort<Integer>();
 
 	@Test
-	void bubbleSortStringTest() {
+	void insertionSortStringTest() {
 		String[] list = {"q", "c", "h", "d", "p", "x"};
 		String[] expectedList = {"c", "d", "h", "p", "q", "x"};
-		stringBubbleSort.sort(list);
+		stringInsertionSort.sort(list);
 		assertArrayEquals(expectedList, list);
 	}
 
 	@Test
-	void bubbleSortIntegerTest() {
+	void insertionSortIntegerTest() {
 		Integer[] list = {5, 8, 3, 6, 1, 9, 2, 7, 4, 0};
 		Integer[] expectedList = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-		integerBubbleSort.sort(list);
+		integerInsertionSort.sort(list);
 		assertArrayEquals(expectedList, list);
 	}
+
 }
