97b419ec313daac9cf3f37abd78b787dcc37e416
Manuel Werder <manuel.werder@csbe.ch>
2019-10-20 12:23:48 +0200

    Using Consumer Interface in SortingMain.java

--- src/main/java/Sort/SortingMain.java
+++ src/main/java/Sort/SortingMain.java
@@ -1,10 +1,12 @@
 package Sort;
 
+import java.util.function.Consumer;
+
 public class SortingMain {
 
-	private static <T> void measureRuntime(T[] list, String sortingName, String typeName, Sort<T> sorter) {
+	private static <T> void measureRuntime(T[] list, String sortingName, String typeName, Consumer<T[]> sorter) {
 		long startTime = System.nanoTime();
-		sorter.sort(list);
+		sorter.accept(list);
 		long stopTime = System.nanoTime();
 		double milliseconds = (stopTime - startTime) / 1000000.0;
 		System.out.println("Runtime of " + sortingName + " on " + typeName + " in milliseconds: " + milliseconds);
@@ -14,22 +16,22 @@ public class SortingMain {
 	public static void main(String[] args) {
 
 		Integer[] integerList1 = {12, 0, 9, 10, 4, 6, 13, 2, 8, 5, -1, 11, 1, 7, 3, 14, -2};
-		measureRuntime(integerList1, "BubbleSort", "Integer", new BubbleSort<Integer>());
+		measureRuntime(integerList1, "BubbleSort", "Integer", list -> new BubbleSort<Integer>().sort(list));
 
 		System.out.println();
 
 		Integer[] integerList2 = {12, 0, 9, 10, 4, 6, 13, 2, 8, 5, -1, 11, 1, 7, 3, 14, -2};
-		measureRuntime(integerList2, "InsertionSort", "Integer", new InsertionSort<Integer>());
+		measureRuntime(integerList2, "InsertionSort", "Integer", list ->  new InsertionSort<Integer>().sort(list));
 
 		System.out.println();
 
 		String[] stringList1 = {"v", "p", "l", "q", "c", "g", "h", "d", "o", "x", "r", "z", "a", "t", "m", "k", "f", "y", "w", "u", "i", "b", "e", "j", "s", "n"};
-		measureRuntime(stringList1, "BubbleSort", "String", new BubbleSort<String>());
+		measureRuntime(stringList1, "BubbleSort", "String", list -> new BubbleSort<String>().sort(list));
 
 		System.out.println();
 
 		String[] stringList2 = {"v", "p", "l", "q", "c", "g", "h", "d", "o", "x", "r", "z", "a", "t", "m", "k", "f", "y", "w", "u", "i", "b", "e", "j", "s", "n"};
-		measureRuntime(stringList2, "InsertionSort", "String", new InsertionSort<String>());
+		measureRuntime(stringList2, "InsertionSort", "String", list -> new InsertionSort<String>().sort(list));
 
 	}
 
