7b88980db860fd9ef5c41203487e59f39b88b7a5
Manuel Werder <manuel.werder@csbe.ch>
2019-10-20 14:41:06 +0200

    QuickSort implemented, 3 minor variations of it

--- src/main/java/Sort/QuickSort.java
+++ src/main/java/Sort/QuickSort.java
@@ -1,10 +1,73 @@
 package Sort;
 
+import java.util.function.Predicate;
+
 public class QuickSort<T extends Comparable<T>> implements Sort<T> {
 
 	@Override
 	public void sort(T[] list) {
-
+		quickSort(list, 0, list.length - 1);
 	}
+
+	private void quickSort(T[] list, int leftSide, int rightSide) {
+		if (leftSide > rightSide){
+			int i = leftSide;
+			int j = rightSide - 1;
+			do {
+				while (list[i].compareTo(list[rightSide]) < 0) i++;
+				while (list[j].compareTo(list[rightSide]) > 0 && j > i) j--;
+				if (i < j) swap(list, i, j);
+			} while (i < j);
+			swap(list, i, rightSide);
+			quickSort(list, leftSide, i - 1);
+			quickSort(list, i + 1, rightSide);
+		}
+	}
+
+	private void swap(T[] list, int i, int j) {
+		T temp = list[i];
+		list[i] = list[j];
+		list[j] = temp;
+	}
+
+//  Version 1
+//	private void quickSort(T[] list, int leftSide, int rightSide) {
+//		if (rightSide > leftSide){
+//			int i = leftSide - 1;
+//			int j = rightSide;
+//			while (true) {
+//				while (list[++i].compareTo(list[rightSide]) < 0);
+//				while (list[--j].compareTo(list[rightSide]) > 0 && j > i);
+//				if (i >= j) break;
+//				swap(list, i, j);
+//			}
+//			swap(list, i, rightSide);
+//			quickSort(list, leftSide, i - 1);
+//			quickSort(list, i +
+//			1, rightSide);
+//		}
+//	}
+
+//	Version 2
+//	private void quickSort(T[] list, int leftSide, int rightSide) {
+//		if (rightSide > leftSide){
+//			int divider = divide(list, leftSide, rightSide);
+//			quickSort(list, leftSide, divider - 1);
+//			quickSort(list, divider + 1, rightSide);
+//		}
+//	}
+//
+//	private int divide(T[] list, int leftSide, int rightSide) {
+//		int i = leftSide;
+//		int j = rightSide - 1;
+//		T pivot = list[rightSide];
+//		do {
+//			while (i < rightSide && list[i].compareTo(pivot) < 0) i++;
+//			while( j > leftSide && list[j].compareTo(pivot) >= 0) j--;
+//			if (i < j) swap(list, i, j);
+//		} while (i < j);
+//		swap(list, i, rightSide);
+//		return i;
+//	}
 
 }
--- src/main/java/Sort/SortingMain.java
+++ src/main/java/Sort/SortingMain.java
@@ -1,8 +1,28 @@
 package Sort;
 
+import java.util.Random;
 import java.util.function.Consumer;
 
 public class SortingMain {
+
+	private final static String[] ABC_LIST = {
+			"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+			"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+	};
+
+	private static String[] getRandomStringList(int size) {
+		Random rand = new Random();
+		String[] list = new String[size];
+		for (int i = 0; i < size; i++) list[i] = ABC_LIST[rand.nextInt(ABC_LIST.length)];
+		return list;
+	}
+
+	private static int[] getRandomIntList(int size) {
+		Random rand = new Random();
+		int[] list = new int[size];
+		for (int i = 0; i < size; i++) list[i] = rand.nextInt();
+		return list;
+	}
 
 	private static <T> void measureRuntime(T[] list, String sortingName, String typeName, Consumer<T[]> sorter) {
 		long startTime = System.nanoTime();
@@ -14,24 +34,35 @@ public class SortingMain {
 	}
 
 	public static void main(String[] args) {
+		System.out.println();
 
 		Integer[] integerList1 = {12, 0, 9, 10, 4, 6, 13, 2, 8, 5, -1, 11, 1, 7, 3, 14, -2};
 		measureRuntime(integerList1, "BubbleSort", "Integer", list -> new BubbleSort<Integer>().sort(list));
 
 		System.out.println();
 
+		String[] stringList1 = getRandomStringList(100);
+		measureRuntime(stringList1, "BubbleSort", "String", list -> new BubbleSort<String>().sort(list));
+
+		System.out.println("\n");
+
 		Integer[] integerList2 = {12, 0, 9, 10, 4, 6, 13, 2, 8, 5, -1, 11, 1, 7, 3, 14, -2};
 		measureRuntime(integerList2, "InsertionSort", "Integer", list ->  new InsertionSort<Integer>().sort(list));
 
 		System.out.println();
 
-		String[] stringList1 = {"v", "p", "l", "q", "c", "g", "h", "d", "o", "x", "r", "z", "a", "t", "m", "k", "f", "y", "w", "u", "i", "b", "e", "j", "s", "n"};
-		measureRuntime(stringList1, "BubbleSort", "String", list -> new BubbleSort<String>().sort(list));
+		String[] stringList2 = getRandomStringList(100);
+		measureRuntime(stringList2, "InsertionSort", "String", list -> new InsertionSort<String>().sort(list));
+
+		System.out.println("\n");
+
+		Integer[] integerList3 = {12, 0, 9, 10, 4, 6, 13, 2, 8, 5, -1, 11, 1, 7, 3, 14, -2};
+		measureRuntime(integerList3, "QuickSort", "Integer", list ->  new QuickSort<Integer>().sort(list));
 
 		System.out.println();
 
-		String[] stringList2 = {"v", "p", "l", "q", "c", "g", "h", "d", "o", "x", "r", "z", "a", "t", "m", "k", "f", "y", "w", "u", "i", "b", "e", "j", "s", "n"};
-		measureRuntime(stringList2, "InsertionSort", "String", list -> new InsertionSort<String>().sort(list));
+		String[] stringList3 = getRandomStringList(100);
+		measureRuntime(stringList3, "QuickSort", "String", list -> new QuickSort<String>().sort(list));
 
 	}
 
